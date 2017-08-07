36d7b8b161c7bbf6db3df202d2eb7ae1b817f999
PandaBot <cuproPanda@users.noreply.github.com>
2017-08-07 07:28:47 -0400

    Initial commit

---
+++ CuproFlags/CuproFlags/Building_Flag.cs
@@ -0,0 +1,86 @@
+﻿using System.Linq;
+using System.IO;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+using System.Collections.Generic;
+
+namespace CuproFlags {
+	[StaticConstructorOnStartup]
+	public class Building_Flag : Building {
+
+		public static Graphic Pole = GraphicDatabase.Get<Graphic_Single>("Cupro/Object/Utility/Flags/Pole", ShaderDatabase.CutoutComplex, new Vector2(3, 3), Color.white, Color.white);
+
+		private Graphic[] animFrames;
+		private Graphic currFrame;
+		private int frameLerp = 0;
+		private string wd;
+		private bool updated;
+		private int swayTicks = 10;
+
+		public override Graphic Graphic {
+			get {
+				if (updated) {
+					return Pole;
+				}
+				return base.Graphic;
+			}
+		}
+
+		private Color Color {
+			get {
+				if (Stuff != null) {
+					return Stuff.stuffProps.color;
+				}
+				return def.graphicData.color;
+			}
+		}
+
+
+		public override void SpawnSetup(Map map, bool respawningAfterLoad) {
+			base.SpawnSetup(map, respawningAfterLoad);
+			wd = Path.GetDirectoryName(Graphic.data.texPath);
+			LongEventHandler.ExecuteWhenFinished(GetGraphicArray);
+		}
+
+
+		public void GetGraphicArray() {
+			animFrames = new Graphic_Single[5];
+			for (int i = 0; i < 5; i++) {
+				animFrames[i] = GraphicDatabase.Get<Graphic_Single>($"{wd}/Anim{i}", ShaderDatabase.ShaderFromType(def.graphicData.shaderType), new Vector2(3, 3), Color.white);
+			}
+			currFrame = animFrames[Rand.Range(0,5)];
+			updated = true;
+		}
+
+
+		public override void Tick() {
+			base.Tick();
+
+			if (this.IsHashIntervalTick(swayTicks)) {
+				frameLerp++;
+				if (frameLerp >= 5) {
+					frameLerp = 0;
+				}
+				currFrame = animFrames[frameLerp];
+			}
+
+			if (this.IsHashIntervalTick(60)) {
+				swayTicks = (int)GenMath.LerpDouble(0.3f, 1.5f, 15f, 5f, Map.windManager.WindSpeed);
+			}
+		}
+
+
+		public override void Draw() {
+			base.Draw();
+			if (currFrame != null) {
+				Matrix4x4 matrix = default(Matrix4x4);
+				Vector3 s = new Vector3(def.graphicData.drawSize.x, 9f, def.graphicData.drawSize.y);
+				Vector3 x = new Vector3(0f, 0f, 0f);
+				matrix.SetTRS(DrawPos + x + Altitudes.AltIncVect, Rotation.AsQuat, s);
+				Graphics.DrawMesh(MeshPool.plane10, matrix, currFrame.GetColoredVersion(ShaderDatabase.ShaderFromType(def.graphicData.shaderType), Color, Color.white).MatSingle, 0);
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Flags
-Adds decorative flags for marking areas or displaying heraldry
+
+Adds decorative flags for marking areas or displaying heraldry.
+
+This is the core mod, and does not add additional logos from Patreon rewards.
+
+![Preview](https://raw.githubusercontent.com/cuproPanda/Flags/master/About/Preview.png)
